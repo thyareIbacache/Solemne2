cac9ad82fc820b6b37e5c5ba757d42955454de09
pablolsddr <pablolores2@gmail.com>
2024-06-10 21:24:22 -0400

    backend

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,17 @@
-import os
+from flask import Flask
+from Backend.config import Config
+from Backend.models import db
+from routes.auth import auth_bp
+from routes.documents import documents_bp
+from routes.search import search_bp
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///udp_apuntes.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'uploads/'
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(documents_bp, url_prefix='/documents')
+app.register_blueprint(search_bp, url_prefix='/search')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Solemne2
 Thyare Ibacache - Ignacia Moya
+
+Informaticos 
+Margarita Osorio - La Hechicera de la Nube
+Marcos Valderrama - El Artífice del Rendimiento
+Pablo Lores - El Guardián del Repositorio
+
+instalaciones 
+pip install Flask 
+pip install Flask-SQLAlchemy 
+pip install Flask-Uploads  
+pip install Flask-OAuthlib Whoosh
+
+BD.txt es el script para iniciar la base de datos 
+esta actualizacion es solo una pequeña base 
+
+
+
