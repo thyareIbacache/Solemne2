05f9c10c3211a6ead37e14d99d486f11c557b4bc
pablolsddr <pablolores2@gmail.com>
2024-06-12 12:25:46 -0400

    mod

--- Backend/app.py
+++ Backend/app.py
@@ -1,17 +1,23 @@
-from flask import Flask
-from Backend.config import Config
-from Backend.models import db
-from routes.auth import auth_bp
-from routes.documents import documents_bp
-from routes.search import search_bp
+from flask import Flask, jsonify
+from config import Config
+from models import db, User
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
-app.register_blueprint(auth_bp, url_prefix='/auth')
-app.register_blueprint(documents_bp, url_prefix='/documents')
-app.register_blueprint(search_bp, url_prefix='/search')
+@app.route('/')
+def home():
+    return "Welcome to UDP Apuntes en Línea!"
+
+@app.route('/test-db')
+def test_db():
+    try:
+        # Realiza una consulta simple
+        users = User.query.all()
+        return jsonify([user.email for user in users])
+    except Exception as e:
+        return str(e), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
