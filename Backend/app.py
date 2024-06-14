ad5ff57096cf33db2b5d0a054e1691c5f2b9cf03
Ubuntu <mrcou@Proyectos-Interdiciplinarios.rdyjy3uken2elcxzbdp11prbxa.bx.internal.cloudapp.net>
2024-06-14 00:50:34 +0000

    -

--- Backend/app.py
+++ Backend/app.py
@@ -1,20 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from config import Config
-from models import db, User
+from models import db, Usuarios
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='HTML')
 app.config.from_object(Config)
 db.init_app(app)
 
 @app.route('/')
 def home():
-    return "Welcome to UDP Apuntes en Línea!"
+    return render_template('Login.html')
 
 @app.route('/test-db')
 def test_db():
     try:
-        users = User.query.all()
-        return jsonify([user.email for user in users])
+        users = Usuarios.query.all()
+        user_data = [
+            {
+                'id_usuario': user.id_usuario,
+                'correo': user.correo,
+                'fecha_registro': user.fecha_registro,
+                'rol': user.rol
+            } for user in users
+        ]
+        return jsonify(user_data)
     except Exception as e:
         return str(e), 500
 
--- Backend/models.py
+++ Backend/models.py
@@ -3,11 +3,13 @@ from config import Config  # Corregir la importación
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
-    role = db.Column(db.String(10))
+class Usuarios(db.Model):
+    __tablename__ = 'usuarios'
+    id_usuario = db.Column(db.Integer, primary_key=True)
+    correo = db.Column(db.String(255), unique=True, nullable=False)
+    contraseña = db.Column(db.String(255), nullable=False)
+    fecha_registro = db.Column(db.TIMESTAMP, default=db.func.current_timestamp())
+    rol = db.Column(db.String(50), nullable=False)
 
 class Document(db.Model):
     id = db.Column(db.Integer, primary_key=True)
