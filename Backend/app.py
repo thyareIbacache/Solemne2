54cc6140b635a5ded39db395eea037af8f7b6948
Ubuntu <mrcou@Proyectos-Interdiciplinarios.rdyjy3uken2elcxzbdp11prbxa.bx.internal.cloudapp.net>
2024-06-16 18:14:27 +0000

    BugFix Perfiles HTML

--- Backend/app.py
+++ Backend/app.py
@@ -114,7 +114,7 @@ def perfil():
         id_usuario = session['id_usuario']
         usuario = Usuarios.query.get(id_usuario)
         archivos = Archivos.query.filter_by(usuario_que_lo_subio=id_usuario).all()
-        return render_template('perfilWebv3.html', usuario=usuario, archivos=archivos)
+        return render_template('PerfilWebv3.html', usuario=usuario, archivos=archivos)
     else:
         return redirect(url_for('home'))
 
