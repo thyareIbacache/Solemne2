ed0a750f70925797405f15657d222d9568237de2
MrC0u <marcosdvalderrama@gmail.com>
2024-07-01 04:26:20 -0400

    fix

--- Backend/app/auth.py
+++ Backend/app/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for, session, current_app
 from .models import Usuarios
 from authlib.integrations.flask_client import OAuth
 import os
