db38cc1ef844d59ee368b754e49bc77bb8343e83
Pablolsddr1 <144722522+Pablolsddr1@users.noreply.github.com>
2024-07-01 21:52:07 -0400

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,27 +1,22 @@
-from flask import Flask, session, render_template, request, redirect, url_for,  send_from_directory
+from flask import Flask, session, render_template, request, redirect, url_for, send_from_directory,jsonify
 from config import Config
 from models import db, Usuarios, Archivos
 from werkzeug.utils import secure_filename
 from werkzeug.security import check_password_hash
 from authlib.integrations.flask_client import OAuth
 from datetime import datetime
-from dotenv import load_dotenv
-load_dotenv()
-
 import os
-
-
 
 app = Flask(__name__, template_folder='HTML')
 app.config.from_object(Config)
 db.init_app(app)
 
-# ruta de carga de archivos
+# Ruta de carga de archivos
 UPLOAD_FOLDER = './uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Tamaño máximo del archivo (en bytes)
 
-@app.route('/uploads/<filename>')
+@app.route('/uploads/<path:filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
@@ -35,7 +30,7 @@ def home():
         contraseña = request.form['password']
         usuario = Usuarios.query.filter_by(correo=correo).first()
 
-        if usuario and (usuario.contraseña == contraseña):
+        if usuario and check_password_hash(usuario.contraseña, contraseña):
             session['id_usuario'] = usuario.id_usuario
             return redirect(url_for('perfil'))
         else:
@@ -44,7 +39,6 @@ def home():
     
     error_message = request.args.get('error', None)
     if error_message:
-    # Procesar el error, mostrarlo en la página, etc.
         return render_template('Login.html', error_message=error_message)
 
     return render_template('Login.html')
@@ -52,97 +46,46 @@ def home():
 oauth = OAuth(app)
 google = oauth.register(
     'google',
-    server_metadata_url=os.getenv('GOOGLE_METADATA_URL'),
+    server_metadata_url=os.getenv('GOOGLE_METADATA_URL'),  # Asegúrate de que estas variables estén definidas en otro lugar
     client_id=os.getenv('GOOGLE_CLIENT_ID'),
     client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),
-    client_kwargs={'scope': 'openid email profile'}
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
 )
 
-# Ingreso con Google
-@app.route('/login-google')
-def login():
-    redirect_uri = url_for('authorize', _external=True)
-    return google.authorize_redirect(redirect_uri)
-
-from flask import redirect, url_for, session
-
-# Prueba
-@app.route('/login/callback')
-def authorize():
-    try:
-        token = google.authorize_access_token()
-    except Exception as e:
-        # Manejar excepción si falla la autorización
-        print(e)
-        return f"{e}"
-        #return redirect(url_for('home', error='Falló la autorización'))
-
-    resp = google.get('https://openidconnect.googleapis.com/v1/userinfo') # Usar el endpoint correcto para obtener información del usuario
-    user_info = resp.json()
-    return f"{user_info}"
-
-
-# Ruta para la carga de archivos
 @app.route('/cargar', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
+def cargar():
+    if 'id_usuario' in session:
         id_usuario = session['id_usuario']
+        user_upload_folder = os.path.join(app.config['UPLOAD_FOLDER'], str(id_usuario))
+        os.makedirs(user_upload_folder, exist_ok=True)  # Crear carpeta si no existe
 
-        # Validar el formulario antes de procesar la carga de archivos
-        nombre_archivo = request.form.get('nombre_archivo')
-        asignatura = request.form.get('asignatura')
-        unidad = request.form.get('unidad')
-        if not nombre_archivo or not asignatura or not unidad:
-            mensaje = 'Por favor, completa todos los campos del formulario.'
-            return render_template('Cargarv2.html', error_message=mensaje, nombre_archivo=nombre_archivo, asignatura=asignatura, unidad=unidad)
+        if request.method == 'POST':
+            archivo = request.files['archivo']
+            if archivo:
+                filename = secure_filename(archivo.filename)
+                archivo.save(os.path.join(user_upload_folder, filename))  # Guardar en la carpeta del usuario
 
-        # Verificar si se ha enviado un archivo en la solicitud
-        if 'archivo' not in request.files:
-            return 'No se encontró el archivo', 400
-
-        file = request.files['archivo']
-
-        # Verificar si no se seleccionó ningún archivo
-        if file.filename == '':
-            mensaje = 'No se seleccionó ningun archivo.'
-            return render_template('Cargarv2.html', error_message=mensaje, nombre_archivo=nombre_archivo, asignatura=asignatura, unidad=unidad)
-
-
-        # Si se recibe un archivo y es válido
-        if file:
-            # Guardar el archivo en el sistema de archivos del servidor
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-            # Obtener la extensión del archivo para determinar su tipo
-            file_extension = os.path.splitext(filename)[1][1:].lower()  # Obtener la extensión y convertir a minúsculas
-
-            # Crear un nuevo objeto Archivos para almacenar en la base de datos
-            nuevo_archivo = Archivos(
-                nombre_archivo=request.form['nombre_archivo'],
-                tipo=file_extension,
-                fecha_subida=datetime.utcnow(),
-                usuario_que_lo_subio=id_usuario,  # (ID del usuario actual)
-                etiquetas='',
-                ruta_archivo=os.path.join(app.config['UPLOAD_FOLDER'], filename),
-                estado='pendiente',
-                asignatura=request.form['asignatura'],
-                unidad=request.form['unidad'],
-            )
-
-            # Agregar el nuevo archivo a la sesión de la base de datos
-            db.session.add(nuevo_archivo)
-
-            try:
-                # Confirmar los cambios en la base de datos
-                db.session.commit()
-                return redirect(url_for('perfil'))
-            except Exception as e:
-                # Manejar cualquier error que pueda ocurrir al guardar en la base de datos
-                return f'Error al guardar en la base de datos: {str(e)}', 500
-
-    # Si se accede a la página de carga de archivos por GET o si hay errores en POST, renderizar el formulario
-    return render_template('Cargarv2.html')
+                # Guardar en la base de datos
+                nuevo_archivo = Archivos(
+                    nombre_archivo=filename,
+                    fecha_subida=datetime.now(),
+                    usuario_que_lo_subio=id_usuario,
+                    etiquetas='',
+                    ruta_archivo=os.path.join(user_upload_folder, filename),
+                    estado='pendiente',
+                    asignatura=request.form['asignatura'],
+                    unidad=request.form['unidad'],
+                )
+                db.session.add(nuevo_archivo)
+                try:
+                    db.session.commit()
+                    return redirect(url_for('perfil'))
+                except Exception as e:
+                    return f'Error al guardar en la base de datos: {str(e)}', 500
+        return render_template('Cargarv2.html')
+    return redirect(url_for('home'))
 
 @app.route('/perfil')
 def perfil():
