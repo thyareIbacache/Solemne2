c38f746b11e30c4a2e4837d7b536dbfa15c79e39
MrC0u <marcosdvalderrama@gmail.com>
2024-07-07 07:38:49 -0400

    Shared update

--- Backend/app/routes/biblioteca.py
+++ Backend/app/routes/biblioteca.py
@@ -17,11 +17,17 @@ def biblioteca_cursos(id_curso):
     cursos = Cursos.query.all()
     return render_template('bibliocursos.html', archivos=archivos, nombre_curso=nombre_curso, cursos=cursos)
 
+
 @biblioteca_bp.route('/shared')
-def shared():
+@biblioteca_bp.route('/shared-<int:id_usuario>')
+def shared(id_usuario):
     if 'id_usuario' in session:
-        id_usuario = session['id_usuario']
+        if id_usuario is None:
+            id_usuario = session['id_usuario']
+        usuario = Usuarios.query.filter_by(id_usuario=id_usuario).first()
+        if usuario.rol == 'moderador' and session['id_usuario'] != usuario.id_usuario:
+            return redirect(url_for('biblioteca.biblioteca'))
         archivos = Archivos.query.filter_by(usuario_que_lo_subio=id_usuario, estado="aprobado").all()
-        return render_template('shared_files.html', archivos=archivos)
+        return render_template('shared_files.html', archivos=archivos, nombre_completo=usuario.nombre_completo)
     else:
         return redirect(url_for('auth.home'))
