f2050f460bddbb295a84d3c9616c0edf5924f177
pablolsddr <pablolores2@gmail.com>
2024-07-07 03:00:21 -0400

    Usuario completo

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -8,7 +8,6 @@ from .routes.biblioteca import biblioteca_bp
 from .routes.anuncios import anuncios_bp
 from .routes.admin import admin_bp
 from .routes.auditoria import auditoria_bp
-from .routes.admin import Notificación_bp
 
 from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
@@ -41,6 +40,5 @@ def create_app():
     app.register_blueprint(anuncios_bp)
     app.register_blueprint(admin_bp)
     app.register_blueprint(auditoria_bp)
-    app.register_blueprint(Notificación_bp)
 
     return app
--- Backend/app/routes/admin.py
+++ Backend/app/routes/admin.py
@@ -1,29 +1,28 @@
 from flask import Blueprint, render_template, request, redirect, url_for, current_app
 from .models import Usuarios, db
-from datetime import datetime
 from werkzeug.security import generate_password_hash
+from datetime import datetime
 
 admin_bp = Blueprint('admin', __name__)
 
-@admin_bp.route('/usuarios', methods=['GET'])
+@admin_bp.route('/usuarios', methods=['GET', 'POST'])
 def get_usuarios():
+    if request.method == 'POST':
+        nombre_completo = request.form['nombre_completo']
+        correo = request.form['correo']
+        rol = request.form['rol']
+        nuevo_usuario = Usuarios(
+            nombre_completo=nombre_completo,
+            correo=correo,
+            rol=rol,
+            contraseña=generate_password_hash('defaultpassword'),
+            nombre_usuario='',
+            biografia='',
+            año_ingreso=datetime.now().year
+        )
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+        return redirect(url_for('admin.get_usuarios'))
+
     usuarios = Usuarios.query.all()
     return render_template('usuarios.html', usuarios=usuarios)
-
-@admin_bp.route('/usuarios', methods=['POST'])
-def add_usuario():
-    nombre_completo = request.form['nombre_completo']
-    correo = request.form['correo']
-    rol = request.form['rol']
-    nuevo_usuario = Usuarios(
-        nombre_completo=nombre_completo,
-        correo=correo,
-        rol=rol,
-        contraseña=generate_password_hash('defaultpassword'),  # Set a default password, user should change it later
-        nombre_usuario='',
-        biografia='',
-        año_ingreso=datetime.now().year
-    )
-    db.session.add(nuevo_usuario)
-    db.session.commit()
-    return redirect(url_for('admin.get_usuarios'))
