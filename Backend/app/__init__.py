2d18dec8acac82ffd30e4c79f543b01688c0763b
Pablolsddr1 <144722522+Pablolsddr1@users.noreply.github.com>
2024-07-07 01:09:06 -0400

    Update __init__.py

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -6,6 +6,8 @@ from .routes.profile import profile_bp
 from .routes.upload import upload_bp
 from .routes.biblioteca import biblioteca_bp
 from .routes.anuncios import anuncios_bp
+from .routes.admin import admin_bp
+from .routes.auditoria import auditoria_bp
 from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
 import os
@@ -35,5 +37,6 @@ def create_app():
     app.register_blueprint(upload_bp)
     app.register_blueprint(biblioteca_bp)
     app.register_blueprint(anuncios_bp)
-
+    app.register_blueprint(admin_bp)
+    app.register_blueprint(auditoria_bp)
     return app
