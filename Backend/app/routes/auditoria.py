459d3e39dc8cf4c10ae7d2dde2285650b170fc8e
Pablolsddr1 <144722522+Pablolsddr1@users.noreply.github.com>
2024-07-07 04:49:37 -0400

    Update auditoria.py

--- Backend/app/routes/auditoria.py
+++ Backend/app/routes/auditoria.py
@@ -1,42 +1,39 @@
-from flask import Blueprint, render_template, request, jsonify, redirect, url_for
-from .models import Archivos, Usuarios, db
-from datetime import datetime
+from flask import Blueprint, render_template, request, jsonify
+from .models import Archivos, db
 
 auditoria_bp = Blueprint('auditoria', __name__)
 
-# Ruta para obtener la lista de archivos pendientes de revisión
-@auditoria_bp.route('/api/archivos', methods=['GET', 'POST'])
+@auditoria_bp.route('/api/archivos', methods=['GET'])
 def get_archivos():
     archivos = Archivos.query.filter_by(estado='pendiente').all()
     archivos_list = [{
         'id': archivo.id_archivo,
         'nombre': archivo.nombre_archivo,
-        'usuario': Usuarios.query.get(archivo.usuario_que_lo_subio).nombre_completo,
+        'usuario': archivo.usuario_que_lo_subio,
         'fecha_carga': archivo.fecha_subida.strftime("%Y-%m-%d %H:%M:%S")
     } for archivo in archivos]
     return jsonify(archivos_list)
 
-# Ruta para aprobar un archivo
 @auditoria_bp.route('/api/archivos/<int:id>/aprobar', methods=['POST'])
 def aprobar_archivo(id):
     archivo = Archivos.query.get(id)
     if archivo:
         archivo.estado = 'aprobado'
+        archivo.auditado = True  # Cambiar a auditado
         db.session.commit()
         return jsonify({'message': 'Archivo aprobado'}), 200
     return jsonify({'message': 'Archivo no encontrado'}), 404
 
-# Ruta para rechazar un archivo
 @auditoria_bp.route('/api/archivos/<int:id>/rechazar', methods=['POST'])
 def rechazar_archivo(id):
     archivo = Archivos.query.get(id)
     if archivo:
         archivo.estado = 'rechazado'
+        archivo.auditado = False  # Asegurar que no esté auditado
         db.session.commit()
         return jsonify({'message': 'Archivo rechazado'}), 200
     return jsonify({'message': 'Archivo no encontrado'}), 404
 
-# Ruta para mostrar la página de auditoría
 @auditoria_bp.route('/auditoria', methods=['GET'])
 def auditoria():
     return render_template('auditoria.html')
