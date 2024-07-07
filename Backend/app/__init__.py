094b2a2d850872fac48402791cedc93142d615a2
pablolsddr <pablolores2@gmail.com>
2024-07-07 02:17:52 -0400

    Notificación

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -8,6 +8,8 @@ from .routes.biblioteca import biblioteca_bp
 from .routes.anuncios import anuncios_bp
 from .routes.admin import admin_bp
 from .routes.auditoria import auditoria_bp
+from .routes.admin import Notificación_bp
+
 from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
 import os
@@ -39,4 +41,6 @@ def create_app():
     app.register_blueprint(anuncios_bp)
     app.register_blueprint(admin_bp)
     app.register_blueprint(auditoria_bp)
+    app.register_blueprint(Notificación_bp)
+
     return app
