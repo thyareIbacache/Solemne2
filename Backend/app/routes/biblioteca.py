11ede9b08ae1b36d68d49c64d9db405c5a4e09ce
MrC0u <marcosdvalderrama@gmail.com>
2024-07-06 21:30:07 -0400

    Update biblioteca

---
+++ Backend/app/routes/biblioteca.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template, session, redirect, url_for, request
+from .models import Usuarios, Archivos, db
+from datetime import datetime
+
+biblioteca_bp = Blueprint('biblioteca', __name__)
+
+@biblioteca_bp.route('/biblioteca')
+def biblioteca():
+    return render_template('biblioteca.html')
