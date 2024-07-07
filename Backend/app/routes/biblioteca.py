5b174467b907dfe6a4d5592018b2b91d2606a830
marymarito <82898754+marymarito@users.noreply.github.com>
2024-07-07 01:06:09 -0400

    changuitos

--- Backend/app/routes/biblioteca.py
+++ Backend/app/routes/biblioteca.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, session, redirect, url_for, request
-from .models import Usuarios, Archivos, db
+from .models import Usuarios, Archivos, db, Cursos
 from datetime import datetime
 
 biblioteca_bp = Blueprint('biblioteca', __name__)
@@ -9,10 +9,11 @@ def biblioteca():
     archivos = Archivos.query.all()
     return render_template('biblioteca.html', archivos=archivos)
 
-@biblioteca_bp.route('/biblioteca-cursos')
-def biblioteca_cursos():
-    archivos = Archivos.query.filter_by(curso='').all()
-    return render_template('bibliocursos.html', archivos=archivos, nombre_curso='')
+@biblioteca_bp.route('/biblioteca-cursos/<int:id_curso>')
+def biblioteca_cursos(id_curso):
+    archivos = Archivos.query.filter_by(curso_id=id_curso).all()
+    nombre_curso = Cursos.query.filter_by(id_curso=id_curso).first().nombre_curso
+    return render_template('bibliocursos.html', archivos=archivos, nombre_curso=nombre_curso)
 
 @biblioteca_bp.route('/shared')
 def shared():
