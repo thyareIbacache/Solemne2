d82d05bd110ef2d9b532a212cf9817bad7200c16
Ubuntu <mrcou@proyectos-interdisciplinarios.x4p10vroi5gujdhjj3y2ndynoh.xx.internal.cloudapp.net>
2024-07-12 22:01:11 +0000

    Cambio gitignore

--- Backend/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') #or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') #or 'sqlite:///udp_apuntes.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAX_CONTENT_LENGTH = 300 * 1024 * 1024  # Tamaño máximo del archivo (en bytes) - 300 MB
-    UPLOAD_FOLDER = './uploads/'
